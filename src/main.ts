c1b1d47f3b035a9a37d09b510ccf0f19afb0bafd
zhaowp151 <zhaowp151@163.com>
2020-12-11 22:05:35 +0800

    重整http

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import http from 'http'
 
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
 const app = createApp(App)
-app.config.globalProperties.$http = http
+
 app.use(store)
 app.use(router)
 app.mount('#app')
