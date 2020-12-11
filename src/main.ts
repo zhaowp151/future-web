e0916de5e0b49231a32c2980efe3b1cd60b89b2b
zhaowp151 <zhaowp151@163.com>
2020-12-11 20:41:55 +0800

    功能添加

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import http from 'http'
 
-createApp(App).use(store).use(router).mount('#app')
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
+const app = createApp(App)
+app.config.globalProperties.$http = http
+app.use(store)
+app.use(router)
+app.mount('#app')
