da914be014519c4a8ce9d9be6fcc756e3842a5b7
zhaowp151 <zhaowp151@163.com>
2020-12-15 00:07:00 +0800

    回复

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,15 @@ import { createApp } from 'vue'
 // Es6Promise.polyfill()
 
 import App from './App.vue'
-// import router from './router'
-// import store from './store'
+import router from './router'
+import store from './store'
 
-// import ElementPlus from 'element-plus'
-// import 'element-plus/lib/theme-chalk/index.css'
-// import locale from 'element-plus/lib/locale/lang/zh-cn'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
 
 const app = createApp(App)
-// app.use(ElementPlus, { locale })
-// app.use(store)
-// app.use(router)
+app.use(ElementPlus, { locale })
+app.use(store)
+app.use(router)
 app.mount('#app')
